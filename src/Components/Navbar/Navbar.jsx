01ac8191a854ed909182dc28b86d4fb6f772bc3e
soumen bera <soumenbera211@gmail.com>
2026-06-21 20:27:32 +0530

    final all commit done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-red-700 rounded-box z-50 mt-3 w-52 p-2 shadow"
             >
               <li>
                 <NavLink to="/">Home</NavLink>
